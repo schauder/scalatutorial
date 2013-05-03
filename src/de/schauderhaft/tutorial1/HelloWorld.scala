75661d9f72741779b7cc2982cb3b1a585342db15
Jens Schauder <jens.schauder@freenet.de>
2013-05-03 15:54:17 +0200

    add a simple Money class

--- src/de/schauderhaft/tutorial1/HelloWorld.scala
+++ src/de/schauderhaft/tutorial1/HelloWorld.scala
@@ -3,5 +3,10 @@ package de.schauderhaft.tutorial1
 object HelloWorld {
     def main(args: Array[String]) {
         println("Hello World")
+
+        val euro = new Money("Euro", 100)
+
     }
 }
+
+class Money(val currency: String, val amount: Int)
