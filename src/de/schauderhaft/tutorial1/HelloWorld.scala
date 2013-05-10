9c1c58a61ec041fbd50fb0f68faff1d3c38319ae
Jens Schauder <jens.schauder@freenet.de>
2013-05-10 14:11:02 +0200

    adding money
    + is a legal operator
    the result type is optional
    throwing an exception actually has the type Nothing
    if isn't a statement, but an expression

--- src/de/schauderhaft/tutorial1/HelloWorld.scala
+++ src/de/schauderhaft/tutorial1/HelloWorld.scala
@@ -11,7 +11,19 @@ object HelloWorld {
         val someMoreMoney = Money("Euro", 100)
 
         println("Are they the same? " + (euro == someMoreMoney))
+
+        // Let's add some money
+        val lots = euro + someMoreMoney
+
+        println("the sum is: " + lots)
     }
 }
 
-case class Money(val currency: String, val amount: Int)
+case class Money(val currency: String, val amount: Int) {
+    // + is a valid method name
+    def +(m2: Money): Money =
+        if (currency == m2.currency)
+            Money(currency, amount + m2.amount)
+        else
+            throw new IllegalArgumentException("Can't add money of different currencies yet")
+}
